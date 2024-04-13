be24b7dd158ba065e76619fe232ef84002d5f071
Vathanak <vathanaksol1605@gmail.com>
2024-04-13 22:12:01 +0700

    first initialization of project

---
+++ src/test/java/co/istad/elearning_rest_api/ELearningRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.elearning_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
