e5b07029787587b74a783dd635dcf2eda28396a3
HoutSovannarith <hout.sovannarith2000@gmail.com>
2024-04-17 20:54:27 +0700

    Update Domain

--- src/main/java/co/istad/elearning_rest_api/domain/Cities.java
+++ src/main/java/co/istad/elearning_rest_api/domain/Cities.java
@@ -5,24 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Set;
-
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-@Table(name = "tbl_countries")
-public class Countries {
+@Table(name = "tbl_cities")
+public class Cities {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String flag;
-    private String iso;
     private String name;
-    private String nice_name;
-    private int num_code;
-    private int phone_code;
 
-    @OneToMany(mappedBy = "country")
-    private Set<Cities> cities;
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Countries country;
 }
